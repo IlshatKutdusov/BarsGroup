8b4b2207f82e892e0a659b5258da6de36e7389a5
IlshatKutdusov <vip.sanki007@bk.ru>
2021-03-31 18:34:39 +0300

    SubsidyCalculation completed

--- SubsidyCalculation/Program.cs
+++ SubsidyCalculation/Program.cs
@@ -6,7 +6,18 @@ namespace SubsidyCalculation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tariff tariff = new Tariff() { ServiceId = 1, HouseId = 1, PeriodBegin = DateTime.UtcNow, PeriodEnd = DateTime.UtcNow, Value = -10};
+            Volume volume = new Volume() { ServiceId = 1, HouseId = 1, Month = DateTime.UtcNow, Value = 1000};
+            SubsidyCalculation subsidyCalculation = new SubsidyCalculation();
+            try {
+                Charge charge = subsidyCalculation.CalculateSubsidy(volume, tariff);
+                Console.WriteLine("{0} {1} {2} {3}", charge.ServiceId, charge.HouseId, charge.Month, charge.Value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error! Message: " + ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ SubsidyCalculation/SubsidyCalculation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SubsidyCalculation
+{
+    public class SubsidyCalculation : ISubsidyCalculation
+    {
+        public event EventHandler<string> OnNotify;
+        public event EventHandler<Tuple<string, Exception>> OnException;
+
+        public Charge CalculateSubsidy(Volume volumes, Tariff tariff)
+        {
+            OnNotify?.Invoke(this, $"Расчёт начат в {DateTime.UtcNow:G}");
+
+            Charge charge = null;
+
+            if (isCorrectData(volumes, tariff))
+            {
+                try
+                {
+                    charge = new Charge();
+                    charge.ServiceId = volumes.ServiceId;
+                    charge.HouseId = volumes.HouseId;
+                    charge.Month = volumes.Month;
+                    charge.Value = volumes.Value * tariff.Value;
+                }
+                catch (Exception ex)
+                {
+                    OnException?.Invoke(this, new Tuple<string, Exception>("Subsidy calculation error! Message: ", ex));
+                    throw;
+                }
+            }
+
+            OnNotify?.Invoke(this, $"Расчёт успешно завершён в {DateTime.UtcNow:G}");
+
+            return charge;
+        }
+
+        private bool isCorrectData(Volume volumes, Tariff tariff)
+        {
+            bool state = true;
+            if (volumes.ServiceId != tariff.ServiceId)
+            {
+                InvalidInput("ServiceId", new Exception("Идентификаторы услуг у объёма и у тарифа не совпадают!"));
+                state = false;
+            }
+            if (volumes.HouseId != tariff.HouseId)
+            {
+                InvalidInput("HouseId", new Exception("Идентификаторы домов у объёма и у тарифа не совпадают!"));
+                state = false;
+            }
+            if (tariff.PeriodBegin.Month <= volumes.Month.Month && volumes.Month.Month <= tariff.PeriodEnd.Month)
+            {
+                InvalidInput("Month", new Exception("Месяц объёма не входит в период действия тарифа!"));
+                state = false;
+            }
+            if (tariff.Value <= 0)
+            {
+                InvalidInput("Tariff Value", new Exception("Нулевое или отрицательное значение тарифа!"));
+                state = false;
+            }
+            if (volumes.Value < 0)
+            {
+                InvalidInput("Volumes Value", new Exception("Отрицательное значение объема!"));
+                state = false;
+            }
+            return state;
+        }
+
+        private void InvalidInput(string message, Exception ex)
+        {
+            OnException?.Invoke(this, new Tuple<string, Exception>(message, ex));
+            throw ex;
+        }
+    }
+}
