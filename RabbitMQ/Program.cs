2299be60f5b68ec15a80b44bbff6e3975069d9c8
SizeR <vip.sanki007@bk.ru>
2021-05-04 19:25:55 +0300

    RabbitMQ: Sending / Receiving a message

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    const string message = "Hello World";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body
+                        );
+
+                    Console.WriteLine(" [x] Отправлено: {0} ", message);
+                }
+            }
+
+            Console.WriteLine("Нажмите [enter] для выхода.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ2/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, args) =>
+                    {
+                        var body = args.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Получено: {0} ", message);
+                    };
+
+                    channel.BasicConsume(
+                        queue: "hello",
+                        autoAck: true,
+                        consumer: consumer
+                        );
+                }
+            }
+
+            Console.WriteLine("Нажмите [enter] для выхода.");
+            Console.ReadLine();
+        }
+    }
+}
